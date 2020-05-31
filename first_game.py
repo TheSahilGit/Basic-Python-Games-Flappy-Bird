8338208e4e4b4ae7115947d81b79c34cf2960bcf
TheSahilGit <66113739+TheSahilGit@users.noreply.github.com>
2020-05-31 13:14:05 +0530

    Update first_game.py

--- first_game.py
+++ first_game.py
@@ -1,3 +1,7 @@
+### This code is written from help of this tutorial: https://www.youtube.com/watch?v=ujOTNg17LjI&list=PLQVvvaa0QuDdLkP8MrOXLe_rKuf6r80KO
+
+
+
 import pygame                                                                   #Import pygame
 import time
 import random
